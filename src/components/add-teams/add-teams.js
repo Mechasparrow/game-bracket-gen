fdfbb9dcc5c27546b57c883a0d9885be28dc4be6
Michael Navazhylau <mikipux7@gmail.com>
2018-05-07 13:33:19 -0500

    added ability to go back and list of inputs

--- src/components/add-teams/add-teams.js
+++ src/components/add-teams/add-teams.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './add-teams.css';
 
+
+// import route Components here
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch,
+    Redirect
+} from 'react-router-dom'
+
 class PlayerInput extends Component {
 
   constructor(props) {
-    super(props)
+      super(props);
+
   }
 
   render() {
     return (
         <div className = "player-input">
-
+            <input className = "form-control" onChange = {this.props.updatePlayerValue.bind(this, this.props.index)}/>
         </div>
-    )
+    );
   }
 
 
@@ -25,37 +36,145 @@ class AddTeams extends Component {
 
     this.generateEmptyPlayerList = this.generateEmptyPlayerList.bind(this);
 
-    this.state = {
-      player_count: this.props.location.state.player_count,
-      players: []
+    var player_count;
+
+    try {
+        player_count = this.props.location.state.player_count;
+    }catch (err) {
+        player_count = 0;
+        console.log(err);
     }
+
+    this.state = {
+      player_count: player_count,
+      players: [],
+      back: false
+    };
 
     var updatedState = this.state;
     updatedState.players = this.generateEmptyPlayerList(this.state.player_count);
 
-    this.setState(updatedState)
 
+    //Function declarations
+    this.setState(updatedState);
+
+    this.updatePlayerValue = this.updatePlayerValue.bind(this);
+
+    this.submitPlayers = this.submitPlayers.bind(this);
+
+
+   this.goBack = this.goBack.bind(this);
+   this.backRedirect = this.backRedirect.bind(this);
+
+  }
+
+
+  backRedirect() {
+
+      var go_back = this.state.back;
+
+      if (go_back) {
+
+          return (<Redirect push to = {{
+                  pathname: '/',
+                  state: {
+                      from : this.props.location,
+                  }
+          }}/>);
+          
+      }
+
+  }
+    
+  goBack() {
+
+      var new_state = this.state;
+      new_state.back = true;
+      this.setState(new_state);
+  }
+
+  updatePlayerValue(index, e) {
+      var player_name = e.target.value;
+
+      var player_list = this.state.players;
+
+      var player_index = index;
+
+      var new_state = this.state;
+
+      player_list[player_index] = player_name;
+
+      new_state.players = player_list;
+
+      this.setState(new_state);
   }
 
   generateEmptyPlayerList(length) {
 
     var player_list = [];
 
-    for (var i = 0; i <= length; i ++) {
-      player_list.push("");
+    for (var i = 0; i < length; i ++) {
+        player_list.push(
+                ""
+        );
     }
 
     return player_list;
 
   }
 
+  generatePlayerInputs() {
+
+      var player_list_length = this.state.players.length;
+
+      var player_inputs = [];
+
+      for (var i = 0; i < player_list_length; i ++) {
+          player_inputs.push ( <PlayerInput index = {i} updatePlayerValue = {this.updatePlayerValue}>
+                   </PlayerInput> );
+      }
+
+      return player_inputs;
+  }
+
+  submitPlayers() {
+      var players = this.state.players;
+
+      alert(players);
+  }
+
   render() {
     return (
-      <div className = "add-teams">
+      <div className = "add-teams container-fluid">
+
+
         <h1>Add Teams</h1>
+
+        
+        <div className = "back-elem">
+          <button onClick = {this.goBack} className = "btn btn-danger">
+            Back
+          </button>
+
+
+          {this.backRedirect()}
+          
+        </div>
+        
+        
         <h2>Player Count {this.state.player_count}</h2>
+
+        <hr></hr>
+
+        <div className = "player-inputs">
+            {this.generatePlayerInputs()}
+        </div>
+
+        <button onClick = {this.submitPlayers} className = "btn btn-success">
+          Submit
+        </button>
       </div>
-    )
+    );
   }
 
 }
